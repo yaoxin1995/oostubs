65f48775277151291f27dbfac065b36e23a13956
Horst Schirmeier <horst@schirmeier.com>
2022-04-07 15:06:55 +0200

    template for task 1

---
+++ user/appl.h
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                         A P P L I C A T I O N                             */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* The Application class defines the (only) application for OOStuBS.         */
+/*****************************************************************************/
+
+#ifndef __application_include__
+#define __application_include__
+
+class Application
+ 
+{
+private:
+	Application (const Application &copy); // prevent copying
+
+public:
+/* Add your code here */ 
+ 
+	void action ();
+};
+
+#endif

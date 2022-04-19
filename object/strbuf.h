ffc0305987ec608af0eaa2defa719d16ae1ae8ca
shiyue <goodwin00103@gmail.com>
2022-04-19 15:16:32 +0200

    optimate

--- object/strbuf.h
+++ object/strbuf.h
@@ -16,13 +16,47 @@
 #ifndef __strbuf_include__
 #define __strbuf_include__
 
+#define DEFAULT_BUF_SIZE 100
 class Stringbuffer {
 private:
+
+	int current_buf_size;
+
+
 	Stringbuffer(const Stringbuffer &copy); // prevent copying
 
 protected:
 	char buffer[DEFAULT_BUF_SIZE];
 /* Add your code here */ 
+public:
+
+	Stringbuffer() 
+	{
+		current_buf_size = 0;
+	}
+
+	int get_buffer_size () 
+	{
+		return current_buf_size;
+	}
+
+	void reset_buffer_size ()
+	{
+		current_buf_size = 0;
+	}
+
+	void put (char c) {
+
+		buffer[current_buf_size++] = c;
+
+		if (current_buf_size == DEFAULT_BUF_SIZE)
+			flush();
+		
+
+	}
+
+	virtual void flush()=0;
+
 };
 
 #endif

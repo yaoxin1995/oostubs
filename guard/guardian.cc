e3f126142fe6a3c6f85ea22b3e6ef8b4dc4915e1
shiyue <goodwin00103@gmail.com>
2022-05-07 20:00:56 +0200

    Guardian finished

--- guard/guardian.cc
+++ guard/guardian.cc
@@ -11,16 +11,22 @@
 
 /* INCLUDES */
 #include "machine/plugbox.h"
+#include "guard/guard.h"
 
 /* FUNCTIONS */
 
 extern "C" void guardian (unsigned int slot);
-
+extern Guard guard;
 /* GUARDIAN: Low-level interrupt handling. We will extend this function at */
 /*           a later point in time.                                        */
 
 extern Plugbox plugbox;
 void guardian (unsigned int slot)
 {   
-    plugbox.report(slot).trigger();
+    Gate &gate = plugbox.report(slot);
+ 
+ //prologue() return ture and request it's epilogue  
+    if(gate.prologue()){
+        guard.relay(&gate);
+    };
 }

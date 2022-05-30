b176a0776bd8e81214f9423afda2db96c85f0668
Horst Schirmeier <horst@schirmeier.com>
2022-05-30 11:45:35 +0200

    template for task 5

---
+++ syscall/guarded_scheduler.h
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                   G U A R D E D _ S C H E D U L E R                       */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Implements the system-call interface to the Scheduler.                    */
+/*****************************************************************************/
+
+#ifndef __guarded_scheduler_include__
+#define __guarded_scheduler_include__
+
+/* Add your code here */ 
+
+class Guarded_Scheduler
+/* Add your code here */ 
+{
+private:
+	Guarded_Scheduler(const Guarded_Scheduler &copy); // prevent copying
+public:
+	Guarded_Scheduler () {}
+/* Add your code here */ 
+};
+
+#endif

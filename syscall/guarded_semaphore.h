4897a6106178e7871a1427a8be858a81f0aa0a2a
Horst Schirmeier <horst@schirmeier.com>
2022-06-13 18:30:12 +0200

    template for task 6

---
+++ syscall/guarded_semaphore.h
@@ -0,0 +1,25 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                   G U A R D E D _ S E M A P H O R E                       */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* The Guarded_Semaphore class implements the system call interface to the   */
+/* Semaphore class.                                                          */
+/*****************************************************************************/
+
+#ifndef __Guarded_Semaphore_include__
+#define __Guarded_Semaphore_include__
+
+/* Add your code here */ 
+
+class Guarded_Semaphore
+/* Add your code here */ 
+{
+private:
+	Guarded_Semaphore(const Guarded_Semaphore &copy); // prevent copying
+/* Add your code here */ 
+};
+
+#endif
---
+++ thread/customer.h
@@ -0,0 +1,25 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                         C U S T O M E R                                   */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* The Customer class extends the Entrant class with the ability to record   */
+/* and retrieve an event that the process in question is waiting for.        */
+/*****************************************************************************/
+
+#ifndef __customer_include__
+#define __customer_include__
+
+/* Add your code here */ 
+
+class Customer
+/* Add your code here */ 
+{
+private:
+	Customer (const Customer &copy); // prevent copying
+/* Add your code here */ 
+};
+
+#endif

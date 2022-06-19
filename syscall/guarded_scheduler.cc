c60be77238fadbbdf57f81586240e2fa0c56ebbb
yaoxin <yaoxinjing517@gmail.com>
2022-06-19 12:05:30 -0700

    finish watch and guarded_scheduler class

--- syscall/guarded_scheduler.cc
+++ syscall/guarded_scheduler.cc
@@ -9,3 +9,44 @@
 /*****************************************************************************/
 
 /* Add your code here */ 
+#include "syscall/guarded_scheduler.h"
+#include "guard/secure.h"
+
+/**
+ * @brief This method registers the process that with the scheduler.
+ * 
+ * @param that 
+ */
+void Guarded_Scheduler::ready (Thread& that) {
+    Secure secure;
+    this->Scheduler::ready (that);
+}
+
+/**
+ * @brief With this method, a process can terminate itself.
+ * 
+ */
+void Guarded_Scheduler::exit () {
+    Secure secure;
+    this->Scheduler::exit();
+}
+
+
+/**
+ * @brief With this method, a process can kill another (that).
+ * 
+ * @param that 
+ */
+void Guarded_Scheduler::kill (Thread& that) {
+    Secure secure;
+    this->Scheduler::kill(that);
+}
+
+/**
+ * @brief This method can be used to trigger a process switch
+ * 
+ */
+void Guarded_Scheduler::resume () {
+    Secure secure;
+    this->Scheduler::resume();
+}
